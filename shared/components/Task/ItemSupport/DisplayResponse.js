af527828b78d6110e8e3d42fa463ca8cb19e378a
Gerda Decio <I>
2017-04-20 15:27:55 +0800

    Add an item summary section

--- shared/components/Task/ItemSupport/DisplayResponse.js
+++ shared/components/Task/ItemSupport/DisplayResponse.js
@@ -4,7 +4,7 @@ export default ({ task }) => {
   const submitForm = (e) => {
     e.preventDefault()
   }
-  
+
   // localhost external_id: 9638d1bbd54b9e9ca057ee2d8052aee8
   // localhost uuid: c6233642-83a3-4d29-be31-e374248d6e57
   // prelive external_id: 8457bdd9-bdd4-4ca0-87a4-daaa55500829
@@ -14,27 +14,45 @@ export default ({ task }) => {
   // let batch_transactions = task.data.batch_transactions || []
   // let itemEvents = task.data.events || []
   console.log(item, timeline)
-  
-  
+
+
   return (
     <div className="container">
       <div className="page-header">
         <h1 id="timeline">{ item.name }</h1>
       </div>
+      <h5 className="section-header">Item Summary</h5>
+      <div className="summary">
+        <div>
+          <small className="text-muted">
+            <label>Marketplace:</label> {item.marketplace_name} <br/>
+            <label>External ID:</label> {item.external_id} <br/>
+            <label>Created at:</label> {item.created_at} <br/>
+            <label>Currency:</label> {item.currency} <br/>
+            <label>Amount:</label> {parseFloat(Math.round((item.amount / 100.00) * 100) / 100).toFixed(2)} <br/>
+            <label>Payment method:</label> {item.payment_method} <br/>
+            <label>Payment type:</label> {item.payment_type_id} <br/>
+            <label>Status:</label> {item.state} <br/>
+            <label>Buyer:</label> {item.buyer_email} <br/>
+            <label>Seller:</label> {item.seller_email} <br/>
+          </small>
+        </div>
+      </div>
+      <h5 className="section-header">Transactions</h5>
       <ul className="timeline">
         {
           timeline.map((t) => {
             if (t.cardType === 'transaction') {
               var fromAccount = t.description.split(" from ")[1].split(" for ")[0]
               var toAccount = t.description.split(" to ")[1].split(" by ")[0]
-              
+
               if (fromAccount === 'Credit Card') fromAccount = `${t.related.transactions[0].user_name}'s ${fromAccount}`
               if (toAccount === 'Wallet Account') toAccount = `${t.related.transactions[0].user_name}'s ${toAccount}`
-              
+
               let amount = parseFloat(Math.round((t.amount / 100.00) * 100) / 100).toFixed(2);
-              
+
               let isFrom = toAccount === 'Item'
-              
+
               return (
                 <li className={ !isFrom ? "timeline-inverted" : null }>
                   <div className="timeline-badge success"><i className="glyphicon glyphicon-thumbs-up"></i></div>
